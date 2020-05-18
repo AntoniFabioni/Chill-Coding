f3d9c85c0a59fe786fb10d89237684e5fc23ffb0
Anthony Fabian <anthony.fabian88@gmail.com>
2020-05-17 21:04:54 -0400

    Implemented way to create grid

--- Percolation-Problem.py
+++ Percolation-Problem.py
@@ -1,1 +1,14 @@
-print("Setup is done, I'll start this next time :)")
+# An N-by-N grid has some proportion of boxes "blocked", and the rest "open".
+# The grid percolated if there is a contiguous path from the top row to the bottom row of open boxes.
+# The goal is to estimate the proportion that leads to near certain percolation.
+
+class Percolation:
+
+    def __init__(self, N):
+        self.N = N
+        self.rows = [[0]*N for i in range(N)]
+
+
+# Test
+fivebyfive = Percolation(5)
+print(fivebyfive.rows)
