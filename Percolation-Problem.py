b6474ed8f80bd5afd4619ea1f9ccfcc9d8f47721
Anthony Fabian <anthony.fabian88@gmail.com>
2020-05-17 21:27:40 -0400

    Added more methods

--- Percolation-Problem.py
+++ Percolation-Problem.py
@@ -6,9 +6,23 @@ class Percolation:
 
     def __init__(self, N):
         self.N = N
-        self.rows = [[0]*N for i in range(N)]
+        self.rows = [[1]*N for i in range(N)]
+    
+    def open(self, x, y):
+        self.rows[x][y] = 0
+
+    def isOpen(self,x,y):
+        return self.open(x,y) == 0
+    
+    def countOpen(self):
+        return self.N**2 - sum([sum(i) for i in self.rows])     # Number of boxes minus sum of elements.
+    
 
 
 # Test
 fivebyfive = Percolation(5)
+fivebyfive.open(1,1)
 print(fivebyfive.rows)
+fivebyfive.open(0,4)
+print(fivebyfive.isOpen(1,1))
+print(fivebyfive.countOpen())
