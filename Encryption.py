314c594db4384019fc28e1d12049ce1f01a5d160
Anthony Fabian <anthony.fabian88@gmail.com>
2020-05-30 16:15:02 -0400

    First test run

--- Encryption.py
+++ Encryption.py
@@ -1,1 +1,10 @@
 # Constructing and testing a message encryption system.
+
+import hashlib
+
+str = "Test"
+
+result = hashlib.sha256(str.encode())
+
+print("The hexadecimal equivalent of SHA256 is : ") 
+print(result.hexdigest())
