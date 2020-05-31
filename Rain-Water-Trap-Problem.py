a4a6e09e0a5177def663c578fb26cbb7e209cbed
Anthony Fabian <anthony.fabian88@gmail.com>
2020-05-31 16:07:28 -0400

    Create Rain-Water-Trap-Problem.py

---
+++ Rain-Water-Trap-Problem.py
@@ -0,0 +1,6 @@
+'''
+Given some array of integers, each respresenting an elevation map of
+rectangular hills of width one, how much many square units of water
+would be trapped after it rains.
+'''
+
