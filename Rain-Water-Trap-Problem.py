f5a9fae08ebab8f8f3380aad818c2507976b69e9
Anthony Fabian <anthony.fabian88@gmail.com>
2020-05-31 16:43:28 -0400

    Update Rain-Water-Trap-Problem.py

--- Rain-Water-Trap-Problem.py
+++ Rain-Water-Trap-Problem.py
@@ -4,3 +4,27 @@ rectangular hills of width one, how much many square units of water
 would be trapped after it rains.
 '''
 
+def RainMeter(heights):
+    
+    total = 0
+    bredth = len(heights)
+    
+    if bredth == 0:
+        return total
+    
+    highLeft = [0]*(bredth + 1)
+
+    for i in range(bredth):
+        highLeft[i + 1] = max(highLeft[i], heights[i])
+    
+    highRight = 0
+
+    for i in reversed(range(bredth - 1)):
+        highRight = max(heights[i], highRight)
+        total += min(highLeft[i], highRight) - heights[i] if min(highLeft[i], highRight) > heights[i] else 0
+    
+    return total
+
+test = [0,-1,0,2,1,-1,-3,4,4,1,0,-2]
+
+print(RainMeter(test))
