bf44423441d4eeba11f082462c896629df0d6b8c
Anthony Fabian <anthony.fabian88@gmail.com>
2020-06-08 21:41:34 -0400

    Added some basic functions

--- Machine-Learning-Test.py
+++ Machine-Learning-Test.py
@@ -2,3 +2,26 @@
 Creating a neural network from scratch that predicts the
 output of a boolean function given sample inputs.
 '''
+
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+training_input = np.array([[0,0,1],[1,1,1],[1,0,1],[0,1,1]])
+
+training_output = np.array([[0,1,1,1]]).T
+
+np.random.seed(1)
+
+weights = 2 * np.random.random((3,1)) - 1
+
+print("Random starting weights are:")
+print(weights)
+
+for i in range(1):
+    input_layer = training_input
+    output_layer = sigmoid(np.dot(input_layer, weights))
+
+print("Outputs are:")
+print(output_layer)
