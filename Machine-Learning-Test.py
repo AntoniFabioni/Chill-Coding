ff092ecb257d0daf696a7de2d77ca1b4a8144558
Anthony Fabian <anthony.fabian88@gmail.com>
2020-06-08 21:57:24 -0400

    Update Machine-Learning-Test.py

--- Machine-Learning-Test.py
+++ Machine-Learning-Test.py
@@ -8,9 +8,12 @@ import numpy as np
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def sigmoid_derivative(x):
+    return np.exp(-x) / (1 + np.exp(-x))**2
+
 training_input = np.array([[0,0,1],[1,1,1],[1,0,1],[0,1,1]])
 
-training_output = np.array([[0,1,1,1]]).T
+training_output = np.array([[0,1,1,0]]).T
 
 np.random.seed(1)
 
@@ -18,10 +21,18 @@ weights = 2 * np.random.random((3,1)) - 1
 
 print("Random starting weights are:")
 print(weights)
+print("\n")
 
-for i in range(1):
+for i in range(20000):
     input_layer = training_input
     output_layer = sigmoid(np.dot(input_layer, weights))
+    error = training_output - output_layer
+    adjustments = error*sigmoid_derivative(output_layer)
+    weights += np.dot(input_layer.T, adjustments)
+
+print("Weights after training are:")
+print(weights)
+print("\n")
 
 print("Outputs are:")
 print(output_layer)
