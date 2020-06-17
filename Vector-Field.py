ea4b0160b26e9d1ab8ac4fe5689df3184c6ca14c
Anthony Fabian <anthony.fabian88@gmail.com>
2020-06-17 15:52:41 -0400

    Update Vector-Field.py

--- Vector-Field.py
+++ Vector-Field.py
@@ -1,7 +1,25 @@
 '''
 This program has the goal of displaying a vector field
 corresponding to a given differential equation.
+
+Note that unit vectors will be displayed.
 '''
 
 import numpy as np
+import matplotlib.pyplot as plt
 
+rangex = 20
+rangey = 20
+
+density = 20
+
+x, y = np.meshgrid(np.linspace(-rangex, rangex, density), np.linspace(-rangey, rangey, density))
+
+dx = 1
+dy = -x/y
+
+unit_dx = dx/np.sqrt(dx**2 + dy**2)
+unit_dy = dy/np.sqrt(dx**2 + dy**2)
+
+plt.quiver(x, y, unit_dx, unit_dy)
+plt.show()
