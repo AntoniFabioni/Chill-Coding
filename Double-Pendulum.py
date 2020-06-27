2743165767ea1b5502f54d0e16fa59d7005383e2
Anthony Fabian <anthony.fabian88@gmail.com>
2020-06-27 10:09:51 -0400

    Update Double-Pendulum.py
    Cleaned up code

--- Double-Pendulum.py
+++ Double-Pendulum.py
@@ -3,8 +3,7 @@ This program simulates the motion of a double
 pendulum system in 2 dimensions.
 
 This code was sourced from Matplotlib.org,
-the specific goal here is to make it run
-in real time.
+and modified for my ease of use.
 '''
 
 
@@ -26,7 +25,7 @@ w1 = 0.0    # initial angular velocity of pendulum 1 (degrees per second)
 w2 = 0.0    # initial angular velocity of pendulum 2 (degrees per second)
 
 # create a time array sampled at "dt" second long steps
-dt = 0.02
+dt = 0.05
 t = np.arange(0, 20, dt)
 
 # initial state
@@ -64,33 +63,6 @@ y1 = -L1*cos(y[:, 0])
 
 x2 = L2*sin(y[:, 2]) + x1
 y2 = -L2*cos(y[:, 2]) + y1
-
-# num1 = -G * (2 * M1 + M2) * sin(th1)
-# num2 = -M2 * G * sin(th1 - 2 * th2)
-# num3 = -2 * sin(th1 - th2) * M2
-# num4 = w2**2 * L2 + w1**2 * L1 * cos(th1 - th2)
-# den = L1 * (2 * M1 + M2 - M2 * cos(2 * th1 - 2 * th2))
-# a1 = (num1 + num2 + num3*num4) / den
-
-# num1 = 2 * sin(th1 - th2)
-# num2 = w1**2 * L1 * (M1 + M2)
-# num3 = G * (M1 + M2) * cos(th1)
-# num4 = w2**2 * L2 * M2 * cos(th1 - th2)
-# den = L2 * (2 * M1 + M2 - M2 * cos(2 * th1 - 2 * th2))
-# a2 = num1 * (num2 + num3 + num4) / den
-
-# w1 += a1
-# w2 += a2
-# th1 += w1
-# th2 += w2
-
-# y = 
-
-# x1 = L1 * sin(th1)
-# y1 = -L1 * cos(th1)
-
-# x2 = L2 * sin(th2) + x1
-# y2 = -L2 * cos(th2) + y1
 
 fig = plt.figure()
 ax = fig.add_subplot(111, autoscale_on=False, xlim=(-(L1+L2), L1+L2), ylim=(-(L1+L2), L1+L2))
