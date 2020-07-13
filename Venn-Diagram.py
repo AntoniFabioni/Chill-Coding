ba1c4f69ee3b2d020c5a2724cff106c1df9bb535
Anthony Fabian <anthony.fabian88@gmail.com>
2020-07-13 19:17:04 -0400

    3 set Venn Diagram works

--- Venn-Diagram.py
+++ Venn-Diagram.py
@@ -1,5 +1,15 @@
 '''
 This program helps visualize data with multiple characteristics.
+The data will be displayed in the form a Venn Diagram
 '''
 
-import pandas
+import matplotlib.pyplot as plt
+from venn import venn
+
+animals = {
+    "Lives on Land": {"Turle", "Llama", "Zebu", "Frog", "Mudskipper", "Hookie"},
+    "Swims Underwater": {"Abba Abba", "Salmon", "Sea Snail", "Flying Fish", "Frog", "Hookie", "Mudskipper"},
+    "Flys in the Sky": {"Ladybug", "Flying Fish", "Butterfly", "Falcon", "Hookie"}
+}
+venn(animals)
+plt.show()
