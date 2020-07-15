ecb45d0f7e85a5fe8ecba7279e461195175e3033
Anthony Fabian <anthony.fabian88@gmail.com>
2020-07-15 17:41:00 -0400

    4-set Venn Diagrams now possible

--- Venn-Diagram.py
+++ Venn-Diagram.py
@@ -7,9 +7,12 @@ import matplotlib.pyplot as plt
 from venn import venn
 
 animals = {
-    "Lives on Land": {"Turle", "Llama", "Zebu", "Frog", "Mudskipper", "Hookie", "Otter"},
-    "Swims Underwater": {"Abba Abba", "Salmon", "Sea Snail", "Flying Fish", "Frog", "Hookie", "Mudskipper"},
-    "Flys in the Sky": {"Ladybug", "Flying Fish", "Butterfly", "Falcon", "Hookie", "Otter"}
+    "Lives on Land": {"Turle", "Llama", "Zebu", "Frog", "Mudskipper", "Hookie", "Otter", "Snake", "Snail", "Dragon"},
+    "Lives Underground": {"Mole", "Mole Cricket", "Cicada", "Snake", "Snail", "Dragon"},
+    "Swims Underwater": {"Abba Abba", "Salmon", "Sea Snail", "Flying Fish", "Frog", "Hookie", "Mudskipper", "Snail", "Dragon"},
+    "Flys in the Sky": {"Ladybug", "Flying Fish", "Butterfly", "Falcon", "Hookie", "Otter", "Cicada", "Dragon"}
+
 }
+
 venn(animals)
 plt.show()
