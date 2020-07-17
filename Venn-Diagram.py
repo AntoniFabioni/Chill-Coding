dbedeedd414e198a0bbf4ef625d4d30e1a533fee
Anthony Fabian <anthony.fabian88@gmail.com>
2020-07-17 10:15:10 -0400

    Simplified categories

--- Venn-Diagram.py
+++ Venn-Diagram.py
@@ -6,14 +6,14 @@ The data will be displayed in the form a Venn Diagram
 import matplotlib.pyplot as plt
 from venn import venn
 
-animals = {
-    "Lives on Land": {"Turle", "Llama", "Zebu", "Frog", "Mudskipper", "Hookie", "Otter", "Snake", "Snail", "Dragon"},
-    "Digs Underground": {"Mole", "Mole Cricket", "Cicada", "Snake", "Snail", "Dragon"},
-    "Swims Underwater": {"Abba Abba", "Salmon", "Sea Snail", "Flying Fish", "Frog", "Hookie", "Mudskipper", "Snail", "Dragon"},
-    "Flies in the Sky": {"Ladybug", "Flying Fish", "Butterfly", "Falcon", "Hookie", "Otter", "Cicada", "Dragon"}
+numbers = {
+    "A": {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+    "B": {0, 2, 4, 6, 8, 10, 12, 14},
+    "C": {0, 3, 6, 9, 12, 15},
+    "D": {0, 4, 8, 12}
 
 }
 
-venn(animals, fmt="{percentage:.1f}%", fontsize=10, legend_loc="upper right")
+venn(numbers, fmt="{percentage:.1f}%", fontsize=10, legend_loc="upper right")
 
 plt.show()
