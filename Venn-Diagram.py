9507135a0d94dbe5c2de25bcec91fb61ab75b87b
Anthony Fabian <anthony.fabian88@gmail.com>
2020-07-19 14:19:00 -0400

    Update Venn-Diagram.py

--- Venn-Diagram.py
+++ Venn-Diagram.py
@@ -10,10 +10,11 @@ numbers = {
     "A": {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
     "B": {0, 2, 4, 6, 8, 10, 12, 14},
     "C": {0, 3, 6, 9, 12, 15},
-    "D": {0, 4, 8, 12}
+    "D": {0, 4, 8, 12},
+    "E": {0, 5, 10, 15}
 
 }
 
-venn(numbers, fmt="{percentage:.1f}%", fontsize=10, legend_loc="upper right")
+venn(numbers, fmt="{percentage:.1f}%", fontsize=8, legend_loc="upper right")
 
 plt.show()
