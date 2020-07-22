52d4fd660f6dc8a23cc0d468423cdbd6d17a1ca8
Anthony Fabian <anthony.fabian88@gmail.com>
2020-07-22 14:17:07 -0400

    Test run

--- FFT.py
+++ FFT.py
@@ -4,3 +4,11 @@ a time-varying signal into its respective frequencies.
 '''
 
 import numpy as np
+import numpy.fft as FFT
+
+# Test
+
+input = [np.sin(np.radians(t)) for t in range(361)]
+
+output = FFT.fft(input)
+print(output)
