bad4d1f2b1c9bbffafc7c1ea4ec93a081da14f16
Anthony Fabian <anthony.fabian88@gmail.com>
2020-07-25 12:37:32 -0400

    Update FFT.py

--- FFT.py
+++ FFT.py
@@ -5,6 +5,7 @@ a time-varying signal into its respective frequencies.
 
 import numpy as np
 import numpy.fft as FFT
+from scipy.signal import argrelextrema
 import matplotlib.pyplot as plt
 
 # Signal
@@ -20,8 +21,12 @@ T = 1.0 / 800.0
 x = np.linspace(0.0, N*T, N)
 y = f(x)
 
+# Transformed x and y
 xf = np.linspace(0.0, 1.0//(2.0*T), N//2)
 yf = FFT.fft(y)
+
+# Local max points
+print(argrelextrema(yf, np.greater))
 
 plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
 plt.grid()
