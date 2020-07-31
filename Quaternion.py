19c7317778b2726d5a5ca3dc661bb195fec2d2bf
Anthony Fabian <anthony.fabian88@gmail.com>
2020-07-31 12:27:42 -0400

    Create Quaternion.py

---
+++ Quaternion.py
@@ -0,0 +1,10 @@
+import numpy
+from pyquaternion import Quaternion
+
+my_quaternion = Quaternion(axis=[1, 0, 0], angle=3.14159265)
+
+numpy.set_printoptions(suppress=True) # Suppress insignificant values for clarity
+v = numpy.array([0., 0., 1.]) # Unit vector in the +z direction
+v_prime = my_quaternion.rotate(v)
+
+print(v_prime)
