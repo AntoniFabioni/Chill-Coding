ac4d631691a56945c66f17f2f1f898be5d8d31ae
Anthony Fabian <anthony.fabian88@gmail.com>
2020-08-02 18:55:59 -0400

    Update Quaternion.py

--- Quaternion.py
+++ Quaternion.py
@@ -1,15 +1,12 @@
-# This program demonstrates various applications of quaternions
+# This program demonstrates various applications of quaternions.
 
 import numpy
 from pyquaternion import Quaternion
 
 my_quaternion = Quaternion(axis=[1, 0, 0], angle=3.14159265)
-my_inverse = my_quaternion.inverse
 
 numpy.set_printoptions(suppress=True) # Suppress insignificant values for clarity
 v = numpy.array([0., 0., 1.]) # Unit vector in the +z direction
 v_prime = my_quaternion.rotate(v)
-v_i = my_inverse.rotate(v)
 
 print(v_prime)
-print(v_i)
