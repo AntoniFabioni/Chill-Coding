821beeeac2f63d95d2fd2a3b26be5f25d47c89e3
Anthony Fabian <anthony.fabian88@gmail.com>
2020-08-06 19:02:59 -0400

    Update Chaos-Equilibrium.py

--- Chaos-Equilibrium.py
+++ Chaos-Equilibrium.py
@@ -4,15 +4,24 @@ def f(x):
     
     return x * (1 - x)
 
-def iterate(start, n):
+def iterate(start, n, r):
 
     ans = start
 
     for i in range(n):
 
-        ans = f(ans)
+        ans = r * f(ans)
 
     return ans
 
 
-print(iterate(0.5, 5))
+print(iterate(0.5, 10, 0))
+print(iterate(0.5, 10, 0.5))
+print(iterate(0.5, 10, 1))
+print(iterate(0.5, 10, 1.5))
+print(iterate(0.5, 10, 2))
+print(iterate(0.5, 10, 2.5))
+print(iterate(0.5, 10, 3))
+print(iterate(0.5, 10, 3.1))
+print(iterate(0.5, 10, 3.2))
+print(iterate(0.5, 10, 3.3))
