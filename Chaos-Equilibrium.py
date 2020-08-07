89afb61f57b80ce8f98eef1116b922ba89d18287
Anthony Fabian <anthony.fabian88@gmail.com>
2020-08-07 13:45:37 -0400

    Update Chaos-Equilibrium.py

--- Chaos-Equilibrium.py
+++ Chaos-Equilibrium.py
@@ -1,10 +1,11 @@
 # This program is under construction.
 
+import numpy as np
 import matplotlib.pyplot as plt
 
-def f(x):
+def f(x, r):
     
-    return x * (1 - x)
+    return r * x * (1 - x)
 
 def iterate(start, n, r):
 
@@ -12,13 +13,16 @@ def iterate(start, n, r):
 
     for i in range(n):
 
-        ans = r * f(ans)
+        ans = f(ans, r)
 
     return ans
 
-x = [i / 100000 for i in range(400000)]
+r = np.linspace(0, 4, 15000)
 
-y = [iterate(0.5, 10, i) for i in x]
+limit = [iterate(0.5, 100, i) for i in r]
 
-plt.plot(x, y)
+plt.plot(r, limit, '-', color='slategrey')
+plt.title('$x_{n + 1} = rx_{n}(1 - x_{n})$', fontsize=20)
+plt.xlabel('r', fontsize=15)
+plt.ylabel('$Limit_{n → ∞}$', fontsize=15) 
 plt.show()
