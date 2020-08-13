71f7338d374ab55a46a48138e8ce797f21645a78
Anthony Fabian <anthony.fabian88@gmail.com>
2020-08-13 11:46:51 -0400

    Update Quaternion.py

--- Quaternion.py
+++ Quaternion.py
@@ -7,7 +7,7 @@ my_quaternion = Quaternion(1, 2, 3, 4)
 my_inverse = my_quaternion.inverse
 
 numpy.set_printoptions(suppress=True) # Suppress insignificant values for clarity
-v = numpy.array([0., 0., 1.]) # Unit vector in the +z direction
+v = numpy.array([0, 0, 1]) # Unit vector in the +z direction
 v_prime = my_quaternion.rotate(v)
 v_undo = my_inverse.rotate(v_prime)
 
