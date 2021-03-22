25fe975de994b9c1bf42d926db7d14fb7043b59b
Anthony Fabian <anthony.fabian88@gmail.com>
2021-03-22 12:42:16 -0400

    Update Complex-Functions.py

--- Complex-Functions.py
+++ Complex-Functions.py
@@ -1,19 +1,16 @@
 # Complex Funnptions
 
 import matplotlib.pyplot as plt
-import numpy as np
-import seaborn
 
-
-vx = np.complex(1, 0)
-vy = np.complex(2, 0)
+vx = complex(1, 0)
+vy = complex(2, 0)
 
 v_in = (vx, vy)
 
 def RootNot(u):
     avg = (u[0] + u[1])/2
     diff = (u[0] - u[1])/2
-    return (np.complex(avg, diff), np.complex(avg, -diff))
+    return (complex(avg, diff), complex(avg, -diff))
 
 v_out = RootNot(v_in)
 
@@ -26,6 +23,5 @@ axs[1, 0].plot(v_out[0].real, v_out[0].imag, 'bx')
 axs[1, 0].set_title('Output: x-component')
 axs[1, 1].plot(v_out[1].real, v_out[1].imag, 'gx')
 axs[1, 1].set_title('Output: y-component')
-
 
 plt.show()
