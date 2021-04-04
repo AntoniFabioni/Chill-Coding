40f34f973c6d6d7ec1647ba260ff72b6aba6856f
Anthony Fabian <anthony.fabian88@gmail.com>
2021-04-04 14:46:39 -0400

    Update Complex-Functions.py

--- Complex-Functions.py
+++ Complex-Functions.py
@@ -22,18 +22,35 @@ def RootNot(u):
 
 fig, axs = plt.subplots(2, 2)
 
-for t in np.linspace(t_start, t_stop, number_of_points):
+vx_real = [rx(t).real for t in np.linspace(t_start, t_stop, number_of_points)]
+vy_real = [ry(t).real for t in np.linspace(t_start, t_stop, number_of_points)]
 
-    vx = rx(t)
-    vy = ry(t)
+vx_imag = [rx(t).imag for t in np.linspace(t_start, t_stop, number_of_points)]
+vy_imag = [ry(t).imag for t in np.linspace(t_start, t_stop, number_of_points)]
+
+vx_out = [RootNot((vx_real[t] + i*vx_imag[t], vy_real[t] + i*vy_imag[t]))[0] for t in range(number_of_points)]
+vy_out = [RootNot((vx_real[t] + i*vx_imag[t], vy_real[t] + i*vy_imag[t]))[1] for t in range(number_of_points)]
+
+# for t in np.linspace(t_start, t_stop, number_of_points):
+
+#     vx = rx(t)
+#     vy = ry(t)
     
-    axs[0, 0].plot(vx.real, vx.imag, 'bo')
-    axs[0, 1].plot(vy.real, vy.imag, 'go')
+#     axs[0, 0].plot(vx.real, vx.imag, 'bo')
+#     axs[0, 1].plot(vy.real, vy.imag, 'go')
 
-    v_out = RootNot((vx, vy))
+#     v_out = RootNot((vx, vy))
 
-    axs[1, 0].plot(v_out[0].real, v_out[0].imag, 'bx')
-    axs[1, 1].plot(v_out[1].real, v_out[1].imag, 'gx')
+#     axs[1, 0].plot(v_out[0].real, v_out[0].imag, 'bx')
+#     axs[1, 1].plot(v_out[1].real, v_out[1].imag, 'gx')
+
+# grad = np.linspace(0,1,number_of_points)
+
+axs[0, 0].plot(vx_real, vx_imag, c='b')
+axs[0, 1].plot(vy_real, vy_imag, c='r')
+
+axs[1, 0].plot(vx_out, vx_out, c='b')
+axs[1, 1].plot(vy_out, vy_out, c='r')
 
 axs[0, 0].set_title('Input: x-component')
 axs[0, 1].set_title('Input: y-component')
